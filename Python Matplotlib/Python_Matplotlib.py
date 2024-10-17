b86440a2ec8c50e241e52f761bbd8e217b3a245a
alberthgrande <alberthgrande98@gmail.com>
2024-10-17 13:48:03 +0800

    [Added] Python Matplotlib

---
+++ Python Matplotlib/Python_Matplotlib.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4]
+y = [10, 20, 25, 30]
+
+# Create a simple plot
+plt.plot(x, y)
+plt.title("Sample Plot")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+
+# Show the plot
+plt.show()
