56114f3bef2c7bb97074947e8452018d862f05d4
alberthgrande <alberthgrande98@gmail.com>
2024-11-03 10:04:31 +0800

    [Added] Python List

---
+++ Python Fundamentals/Python Lists/python_access.py
@@ -0,0 +1,19 @@
+"""
+Access Items
+List items are indexed and you can access them by referring to the index number:
+
+Note: The first item has index 0.
+"""
+
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist[1])
+
+
+"""
+Negative Indexing
+Negative indexing means start from the end
+
+-1 refers to the last item, -2 refers to the second last item etc.
+"""
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
